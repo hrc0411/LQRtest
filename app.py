f9513b51ea51aa5f3d8c544d1dcc9aaed4bcd816
hrc0411 <83512167+hrc0411@users.noreply.github.com>
2023-08-29 13:55:27 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from waitress import serve
+from dash_trial4 import app
+
+if __name__ == '__main__':
+    serve(app.server, host='0.0.0.0', port=8050)
+
+
+# In[ ]:
+
+
+
+
